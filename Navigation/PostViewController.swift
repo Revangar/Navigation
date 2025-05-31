c69eacf913e8ce72527412d1871b59693aa44137
Ilya <incarnadinekulio@gmail.com>
2025-05-31 12:54:28 +0300

    HW2: реализован ProfileHeaderView и ProfileViewController

---
+++ Navigation/PostViewController.swift
@@ -0,0 +1,30 @@
+import UIKit
+
+class PostViewController: UIViewController {
+
+    let post: Post
+
+    init(post: Post) {
+        self.post = post
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = post.title
+        view.backgroundColor = .systemYellow
+
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Info", style: .plain, target: self, action: #selector(showInfo))
+    }
+
+    @objc func showInfo() {
+        let infoVC = InfoViewController()
+        let navVC = UINavigationController(rootViewController: infoVC)
+        navVC.modalPresentationStyle = .fullScreen
+        present(navVC, animated: true)
+    }
+}
