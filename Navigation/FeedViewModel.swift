9961dfdfc9815a10944de510ebc2a645ec422028
Revangar <incarnadinekulio@gmail.com>
2026-08-24 14:56:56 +0300

    Реализация домашнего задания: MVVM

---
+++ Navigation/FeedViewModel.swift
@@ -0,0 +1,41 @@
+import Foundation
+import StorageService
+
+final class FeedViewModel {
+
+    enum GuessState {
+        case idle
+        case empty
+        case correct
+        case incorrect
+    }
+
+    private let model: FeedModel
+
+    private(set) var state: GuessState = .idle {
+        didSet {
+            onStateChanged?(state)
+        }
+    }
+
+    var onStateChanged: ((GuessState) -> Void)?
+
+    init(model: FeedModel) {
+        self.model = model
+    }
+
+    func checkGuess(_ word: String?) {
+        let normalizedWord = word?.trimmingCharacters(in: .whitespacesAndNewlines) ?? ""
+
+        guard !normalizedWord.isEmpty else {
+            state = .empty
+            return
+        }
+
+        state = model.check(word: normalizedWord) ? .correct : .incorrect
+    }
+
+    func post(at index: Int) -> Post? {
+        model.post(at: index)
+    }
+}
